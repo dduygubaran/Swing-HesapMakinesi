d6dad8cc25cd796442fd6bf3707fb1f4a3ce6cca
Duygu Baran <83429611+dduygubaran@users.noreply.github.com>
2022-07-06 14:16:52 +0300

    Add files via upload

---
+++ src/org/o7planning/tutorial/helloosgi/mathservice/impl/MathServiceImpl.java
@@ -0,0 +1,18 @@
+package org.o7planning.tutorial.helloosgi.mathservice.impl;
+
+import org.o7planning.tutorial.helloosgi.mathservice.MathService;
+
+public class MathServiceImpl implements MathService {
+
+	@Override
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+	@Override
+	public int divide(int a, int b) {
+		// TODO Auto-generated method stub
+		return a/b;
+	}
+
+}
